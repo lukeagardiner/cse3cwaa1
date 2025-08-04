6e6d3b1567edb8f44c54e4adbcab7db76199a9eb
Lucas G <lukeagardiner@yahoo.com.au>
2025-08-04 23:41:28 +1000

    adding tab headings to homepage along with hamburger menu. will create feature branch for next update

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 // app/page2.tsx
 import HamburgerMenu from './Components/HamburgerMenu';
+import TabMenu from './Components/TabMenu';
 
 export default function Home() {
     return (
         <div>
             <HamburgerMenu />
+            <TabMenu />
             <h1>Welcome to My Next.js App</h1>
         </div>
     )
